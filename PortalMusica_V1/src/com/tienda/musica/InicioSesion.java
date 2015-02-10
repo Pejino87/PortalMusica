96047cf173b0e19c0e02f66a0883a9b8a180aa33
USUARIO <USUARIO@PC1407>
2015-02-10 16:34:06 +0100

    iniciosesion importar paquetes

--- PortalMusica_V1/src/com/tienda/musica/InicioSesion.java
+++ PortalMusica_V1/src/com/tienda/musica/InicioSesion.java
@@ -9,6 +9,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import com.backend.*;
+import gft.curso.principalempresa.*;
 
 /**
  * Servlet implementation class InicioSesion
