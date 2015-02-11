9981d884da32de636a912e5bf2de4081a91563cf
USUARIO <USUARIO@PC1406>
2015-02-11 16:08:00 +0100

    alvaro-nuevo servlet EliminarLista

---
+++ PortalMusica_V1/src/gft/curso/operacionesempresario/EliminarLista.java
@@ -0,0 +1,37 @@
+package gft.curso.operacionesempresario;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class EliminarLista
+ */
+public class EliminarLista extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public EliminarLista() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request,response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("ViewEliminarLista.jsp").forward(request, response);
+	}
+
+}
